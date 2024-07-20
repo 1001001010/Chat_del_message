2fa5fa6ec61970e04a182449b6fa13a273c1cdc9
Supp$ <1w.supp44@gmail.com>
2024-07-20 08:03:00 +0800

    fix

--- bot/handlers/main_start.py
+++ bot/handlers/main_start.py
@@ -2,4 +2,7 @@ from bot.data.loader import dp, bot
 
 @dp.message_handler(content_types = ['new_chat_members', 'left_chat_member'])
 async def delete(message):
-    await bot.delete_message(message["chat"]["id"], message["message_id"])
+    try:
+        await bot.delete_message(message["chat"]["id"], message["message_id"])
+    except:
+        pass
